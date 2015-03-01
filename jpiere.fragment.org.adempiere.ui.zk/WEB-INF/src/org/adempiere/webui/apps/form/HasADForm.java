361a08da9b7554f484d7be37f27772bfc3bab976
Hideaki Hagiwara <Hideaki Hagiwara@localhost>
2015-03-01 23:03:58 +0900

    ?????

--- jpiere.fragment.org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/form/HasADForm.java
+++ jpiere.fragment.org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/form/HasADForm.java
@@ -1,3 +1,15 @@
+/******************************************************************************
+ * Product: JPiere(ジェイピエール) - JPiere Fragments                         *
+ * Copyright (C) Hideaki Hagiwara All Rights Reserved.                        *
+ * このプログラムはGNU Gneral Public Licens Version2のもと公開しています。    *
+ * このプログラムは自由に活用してもらう事を期待して公開していますが、         *
+ * いかなる保証もしていません。                                               *
+ * 著作権は萩原秀明(h.hagiwara@oss-erp.co.jp)が保持し、サポートサービスは     *
+ * 株式会社オープンソース・イーアールピー・ソリューションズで                 *
+ * 提供しています。サポートをご希望の際には、                                 *
+ * 株式会社オープンソース・イーアールピー・ソリューションズまでご連絡下さい。 *
+ * http://www.oss-erp.co.jp/                                                  *
+ *****************************************************************************/
 package org.adempiere.webui.apps.form;
 
 import org.adempiere.webui.adwindow.ADWindow;
